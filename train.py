49857d350ada2b279faa01c2682d0ec1883b0754
郑博培 <58476312+zbp-xxxp@users.noreply.github.com>
2021-10-14 10:39:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -234,7 +234,7 @@ def main():
 
     parser.add_argument("--img_size", default=384, type=int,
                         help="Resolution size")
-    parser.add_argument("--train_batch_size", default=512, type=int,
+    parser.add_argument("--train_batch_size", default=128, type=int,
                         help="Total batch size for training.")
     parser.add_argument("--eval_batch_size", default=64, type=int,
                         help="Total batch size for eval.")
